acba6129771272cfb0059942bff4dda67c20f5de
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-06-13 19:23:41 -0700

    refactoring

--- server.js
+++ server.js
@@ -6,48 +6,58 @@ const connection = require("./config/connection");
 const prompt = require("./config/prompts");
 require("console.table");
 
-function viewAllDepartments() {
-  const query = `SELECT * FROM department
-  ORDER BY id;`;
-  connection.query(query, (err, res) => {
-      if (err) throw err;
-      console.log('\n');
-      console.log('VIEW ALL DEPARTMENTS');
-      console.log('\n');
-      console.table(res);
-      prompt();
-  });
-}
-
-function viewAllRoles() {
-    const query = `SELECT * FROM role
-    ORDER BY id;`;
-    connection.query(query, (err, res) => {
-        if (err) throw err;
-        console.log('\n');
-        console.log('VIEW ALL ROLES');
-        console.log('\n');
-        console.table(res);
-        prompt();
+function firstPrompt() {
+    // Main Menu
+    inquirer.prompt(prompt.firstPrompt).then(function ({ task }) {
+        switch (task) {
+            case "View Employees":
+                viewEmployee();
+                break;
+            case "View Employees by Manager":
+                viewEmployeeByManager();
+                break;
+            case "View Employees by Department":
+                viewEmployeeByDepartment();
+                break;
+            case "View Departments":
+                viewDepartments();
+                break;
+            case "View Roles":
+                viewRoles();
+                break;
+            case "View Department Budget":
+                viewDepartmentBudget();
+                break;
+            case "Add Employee":
+                addEmployee();
+                break;
+            case "Add Department":
+                addDepartment();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "Update Employee Role":
+                updateEmployeeRole();
+                break;
+            case "Update Employee Manager":
+                updateEmployeeManager();
+                break;
+            case "Remove Employee":
+                deleteEmployee();
+                break;
+            case "Remove Department":
+                deleteDepartment();
+                break;
+            case "Remove Role":
+                deleteRole();
+                break;
+            case "Exit":
+                console.log(
+                    "goodbye!",
+                );
+                connection.end();
+                break;
+        }
     });
-  }
-
-function viewAllEmployees() {
-    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
-    FROM employee
-    LEFT JOIN employee manager on manager.id = employee.manager_id
-    INNER JOIN role ON (role.id = employee.role_id)
-    INNER JOIN department ON (department.id = role.department_id)
-    ORDER BY employee.id;`;
-    connection.query(query, (err, res) => {
-        if (err) throw err;
-        console.log('\n');
-        console.log('VIEW ALL EMPLOYEES');
-        console.log('\n');
-        console.table(res);
-        prompt();
-    });
 }
-
-
-
